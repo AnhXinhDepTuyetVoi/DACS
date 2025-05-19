c32e6dae967922cbdaf354ddd6f6a1ade10b9f32
AnhXinhDepTuyetVoi <duongduong100505@gmail.com>
2025-05-19 12:40:53 +0700

    Add full content of PhanMemGhiChu

---
+++ PhanMemGhiChu/src/main/java/com/mycompany/phanmemghichu/PhanMemGhiChu.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.phanmemghichu;
+
+/**
+ *
+ * @author ANH
+ */
+public class PhanMemGhiChu {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
